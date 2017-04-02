172903c7fd922521150b6afc4b33daa4ad8abc7a
Tony Fabery <tfabery@gmail.com>
2017-04-02 00:13:32 -0700

    add devise user model and landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,8 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    if current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 end
